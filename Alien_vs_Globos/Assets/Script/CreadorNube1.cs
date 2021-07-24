e1e8e88d17c8397d56573a0f912593b6899bb89d
JoseUpia <upia915@gmail.com>
2021-07-23 23:24:10 -0400

    Primeros pasos del menú principal

---
+++ Alien_vs_Globos/Assets/Script/CreadorNube1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreadorNube1 : MonoBehaviour
+{
+    public GameObject Nubes;
+    public float tiempoCreacion = 2f, espacioCreacion = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("Creando", 0.0f, tiempoCreacion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Destroy(Nubes,5f);
+    }
+
+    public void Creando()
+    {
+        Vector2 PosicionCreacion = new Vector2(0,0);
+        PosicionCreacion = this.transform.position + Random.onUnitSphere * espacioCreacion;
+        PosicionCreacion = new Vector2 (this.transform.position.x, PosicionCreacion.y);
+
+        GameObject Nube = Instantiate (Nubes, PosicionCreacion, Quaternion.identity);
+    }
+}
