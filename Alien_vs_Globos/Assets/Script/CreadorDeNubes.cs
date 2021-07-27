061bf85f0cff2528d6ebb7bd8949b147b7167f84
elianmtg <elianmtoribio@gmail.com>
2021-07-27 14:04:23 -0400

    evadiendo cambios de v2021wx

---
+++ Alien_vs_Globos/Assets/Script/CreadorDeNubes.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreadorDeNubes : MonoBehaviour
+{
+    public GameObject Nubes;
+    public float tiempoCreacion = 10f, espacioCreacion = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Creando", 0.0f, tiempoCreacion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Creando()
+    {
+        Vector2 PosicionCreacion = new Vector2(0, 0);
+        PosicionCreacion = this.transform.position + Random.onUnitSphere * espacioCreacion;
+        PosicionCreacion = new Vector2(this.transform.position.x, PosicionCreacion.y);
+
+        GameObject Nube = Instantiate(Nubes, PosicionCreacion, Quaternion.identity);
+
+
+        Destroy(Nube, 25f);
+    }
+}
+
