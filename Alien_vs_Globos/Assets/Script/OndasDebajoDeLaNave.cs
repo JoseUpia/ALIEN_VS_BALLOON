0fb565ac3476859f262ca03c970ef01f8076184d
JoseUpia <upia915@gmail.com>
2021-07-28 00:18:18 -0400

    Cambios de Upia, Perfeccionando el menu

---
+++ Alien_vs_Globos/Assets/Script/OndasDebajoDeLaNave.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OndasDebajoDeLaNave : MonoBehaviour
+{
+    public GameObject Nave;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
